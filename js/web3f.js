97fdbb7c77490d9531f33ca77a8df743a2cbcb92
roundgrounds <76159628+roundgrounds@users.noreply.github.com>
2023-02-04 15:41:57 +0530

    Add files via upload

---
+++ js/web3f.js
@@ -0,0 +1,49 @@
+const form = document.getElementById("kc-form");
+const result = document.getElementById("kc-fs-result");
+
+form.addEventListener("submit", function (e) {
+  const formData = new FormData(form);
+  e.preventDefault();
+  var object = {};
+  formData.forEach((value, key) => {
+    object[key] = value;
+  });
+  var json = JSON.stringify(object);
+  result.innerHTML = "Please wait...";
+
+  fetch("https://api.web3forms.com/submit", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json"
+    },
+    body: json
+  })
+    .then(async (response) => {
+      let json = await response.json();
+      if (response.status == 200) {
+        result.innerHTML = json.message;
+        result.classList.remove("fs-result-gray");
+        result.classList.add("fs-result-green");
+        // result.classList.remove("text-gray-500");
+        // result.classList.add("text-green-500");
+      } else {
+        console.log(response);
+        result.innerHTML = json.message;
+        result.classList.remove("fs-result-gray");
+        result.classList.add("fs-result-red");
+        // result.classList.remove("text-gray-500");
+        // result.classList.add("text-red-500");
+      }
+    })
+    .catch((error) => {
+      console.log(error);
+      result.innerHTML = "Something went wrong!";
+    })
+    .then(function () {
+      form.reset();
+      setTimeout(() => {
+        result.style.display = "none";
+      }, 5000);
+    });
+});
